b316c6e5d78132f45cf84203959ebf2628c1d996
pavel <pavel.stanislavovich.ge@gmail.com>
2026-07-14 21:36:55 +0400

    ip changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -62,7 +62,7 @@ type ApiScheduleRule = {
   endTime?: string;
 };
 
-const BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_URL ?? "http://192.168.100.90:8080";
 
 export const apiClient: AxiosInstance = axios.create({
   baseURL: BASE_URL,
